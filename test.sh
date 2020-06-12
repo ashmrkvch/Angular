f0e04ccfddd1c2da86011d1f63c5a367815925ca
ashmrkvch <shemrikovych@gmail.com>
2020-06-12 15:58:30 +0300

    aaaa

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ docker build -t travis-ci-build-stages-demo .
 docker images
 docker tag travis-ci-build-stages-demo travis-ci-build-stages-demo 
 gcloud docker push gcr.io/airy-ceremony-274018/travis-ci-build-stages-demo       
-#docker run -d --rm travis-ci-build-stages-demo
+docker run -d --rm travis-ci-build-stages-demo
