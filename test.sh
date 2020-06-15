de1ef61946e30d783bcd0573dc809f851f64ffde
ashmrkvch <shemrikovych@gmail.com>
2020-06-15 16:31:23 +0300

    aaa

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 docker build -t travis-ci-build-stages-demo .
 
-echo $keyfile | base64 --decode -i > ${HOME}/${keyfile}
+echo $keyfile | base64 --decode -i > ${HOME}/studious-set-280411-db6dcfed0d74.json
 
-gcloud auth activate-service-account --key-file ${HOME}/${keyfile}
+gcloud auth activate-service-account --key-file ${HOME}/studious-set-280411-db6dcfed0d74.json
 gcloud --quiet config set project studious-set-280411
 
 
@@ -13,6 +13,6 @@ docker tag travis-ci-build-stages-demo travis-ci-build-stages-demo
 
 gcloud docker push gcr.io/studious-set-280411/travis-ci-build-stages-demo     
 
-yes | gcloud beta container images add-tag gcr.io/studious-set-280411/travis-ci-build-stages-demo :$TRAVIS_COMMIT gcr.io/studious-set-280411}/travis-ci-build-stages-demo :latest
+yes | gcloud beta container images add-tag gcr.io/studious-set-280411/travis-ci-build-stages-demo :$TRAVIS_COMMIT gcr.io/studious-set-280411/travis-ci-build-stages-demo :latest
 
 docker run -d --rm travis-ci-build-stages-demo
