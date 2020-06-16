f59e50bc7b72486df6caf1e32942f97fd124d6d2
ashmrkvch <shemrikovych@gmail.com>
2020-06-16 13:32:34 +0300

    aaa

--- test.sh
+++ test.sh
@@ -3,11 +3,8 @@ docker build -t gcr.io/studious-set-280411/travis-ci-build-stages-demo .
 gcloud auth activate-service-account --key-file ${HOME}/studious-set-280411-db6dcfed0d74.json
 gcloud --quiet config set project studious-set-280411
 gcloud docker -- push gcr.io/studious-set-280411/travis-ci-build-stages-demo
-yes | gcloud beta compute instances create my-instance \
-            --project=studious-set-280411 \
-            --zone us-east1-b \
-            --source-machine-image gcr.io/studious-set-280411/travis-ci-build-stages-demo \
-            --service-account travis@studious-set-280411.iam.gserviceaccount.com
+yes | gcloud compute instances create-with-container my-instance \
+          --container-image gcr.io/studious-set-280411/travis-ci-build-stages-demo
       
 
 
