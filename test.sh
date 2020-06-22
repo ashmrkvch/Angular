3e59a42959611a86370ed709d6b55b2460f1df06
ashmrkvch <shemrikovych@gmail.com>
2020-06-22 15:43:24 +0300

    kk

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ gcloud docker -- push gcr.io/studious-set-280411/travis-ci-build-stages-demo
 #yes | gcloud compute instances create-with-container my-instance  --zone us-east1-b --container-image=gcr.io/studious-set-280411/travis-ci-build-stages-demo --tags=http-server
 #gcloud compute firewall-rules create "http-rule" --allow=tcp:80 --source-ranges="0.0.0.0/0" --target-tags=http-server
 yes | gcloud compute instances update-container my-instance  --zone us-east1-b --container-image=gcr.io/studious-set-280411/travis-ci-build-stages-demo 
-gcloud compute ssh --project studious-set-280411 --zone us-east1-b my-instance
+#gcloud compute ssh --project studious-set-280411 --zone us-east1-b my-instance
 sleep 12
 ssh -o StrictHostKeyChecking=no -i deploy_key anhelina@34.75.250.168 
 docker rmi $(docker images -a -q)
