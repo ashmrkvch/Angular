b967372342a41aab3c0498251cad754a40c68880
ashmrkvch <shemrikovych@gmail.com>
2020-06-22 16:40:38 +0300

    kk

--- test.sh
+++ test.sh
@@ -8,5 +8,5 @@ gcloud docker -- push gcr.io/studious-set-280411/travis-ci-build-stages-demo
 yes | gcloud compute instances update-container my-instance  --zone us-east1-b --container-image=gcr.io/studious-set-280411/travis-ci-build-stages-demo 
 gcloud compute ssh --project studious-set-280411 --zone us-east1-b my-instance
 #sleep 12
-ssh -o StrictHostKeyChecking=no -i deploy_key anhelina@34.75.250.168 
+ssh -o StrictHostKeyChecking=no -i deploy_key anhelina@34.75.250.168 -v
 docker rmi $(docker images -a -q)
