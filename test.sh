278c4987ebbe43091d1382526560cb09fab3a5cb
ashmrkvch <shemrikovych@gmail.com>
2020-06-23 11:20:33 +0300

    prosti_gospodi

--- test.sh
+++ test.sh
@@ -8,5 +8,5 @@ gcloud docker -- push gcr.io/studious-set-280411/travis-ci-build-stages-demo
 yes | gcloud compute instances update-container my-instance  --zone us-east1-b --container-image=gcr.io/studious-set-280411/travis-ci-build-stages-demo 
 
 sleep 12
-ssh -o StrictHostKeyChecking=no -i id_rsa.pub anhelina@35.211.249.83 -v
-docker rmi $(docker images -a -q)
+ssh -o StrictHostKeyChecking=no -i id_rsa anhelina@35.211.249.83 -v
+docker image prune -a -f
