bbe6fcd022c0ea1460f2d2e02328a2827fdba9b5
ashmrkvch <shemrikovych@gmail.com>
2020-06-24 15:23:11 +0300

    kkk

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 gcloud --quiet config set project studious-set-280411
+gcloud auth activate-service-account --key-file ${HOME}/studious-set-280411-db6dcfed0d74.json
 gsutil cp gs://bucket-test__anhelina/test.txt /home/angular/test.txt
 docker build -t gcr.io/studious-set-280411/travis-ci-build-stages-demo .
-gcloud auth activate-service-account --key-file ${HOME}/studious-set-280411-db6dcfed0d74.json
+
 
 gcloud docker -- push gcr.io/studious-set-280411/travis-ci-build-stages-demo
 #yes | gcloud compute instances create-with-container my-instance  --zone us-east1-b --container-image=gcr.io/studious-set-280411/travis-ci-build-stages-demo --tags=http-server
