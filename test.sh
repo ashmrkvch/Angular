344b766b3e7cb2f27bb3754f511d8533306ef0bb
ashmrkvch <shemrikovych@gmail.com>
2020-06-30 13:27:27 +0300

    prosto zdravstvui

--- test.sh
+++ test.sh
@@ -13,9 +13,14 @@ gcloud docker -- push gcr.io/studious-set-280411/travis-ci-build-stages-demo
 #yes | gcloud compute instances create-with-container my-instance  --zone us-east1-b --container-image=gcr.io/studious-set-280411/travis-ci-build-stages-demo --tags=http-server
 #gcloud compute firewall-rules create "http-rule" --allow=tcp:80 --source-ranges="0.0.0.0/0" --target-tags=http-server
 #yes | gcloud compute instances update-container my-instance  --zone us-east1-b --container-image=gcr.io/studious-set-280411/travis-ci-build-stages-demo 
-kubectl apply -f pod-test.yaml
-sleep 12
-kubectl get pod test-travis
+#kubectl apply -f pod-test.yaml
+kubectl apply -f web-deployment.yaml
+kubectl apply -f backend.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
+sleep 100
+curl http://34.98.86.43/
+#kubectl get pod test-travis
 #kubectl exec -ti test-travis --  /bin/sh 
 # ssh -o StrictHostKeyChecking=no -i id_rsa anhelina@35.211.249.83 -v
 #docker image prune -a -f
